a5314dde64c934d299a16c81edf6a3aeded6a684
Joao Lourenco <joaodslourenco@gmail.com>
2023-07-03 10:12:12 -0300

    add post service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PostsRepository } from './repositories/posts.repository';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
-  }
+    constructor(private readonly postsRepository: PostsRepository) {}
 
-  findAll() {
-    return `This action returns all posts`;
-  }
+    create(createPostDto: CreatePostDto) {
+        return this.postsRepository.create(createPostDto);
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
-  }
+    findAll() {
+        return this.postsRepository.findAll();
+    }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
+    async findOne(id: number) {
+        const user = await this.postsRepository.findOne(id);
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
-  }
+        if (!user) {
+            throw new NotFoundError('Usuário não encontrado!');
+        }
+        return user;
+    }
+
+    update(id: number, updatePostDto: UpdatePostDto) {
+        return this.postsRepository.update(id, updatePostDto);
+    }
+
+    remove(id: number) {
+        return this.postsRepository.remove(id);
+    }
 }
