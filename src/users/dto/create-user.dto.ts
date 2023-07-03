e31bf8cbfd6233bc8d9a6c4d68f57e66745e094a
Joao Lourenco <joaodslourenco@gmail.com>
2023-07-03 14:10:35 -0300

    add info to swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+    @ApiProperty({ description: 'User e-mail' })
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({ description: 'User name' })
     @IsString()
     @IsNotEmpty()
     name: string;
 
+    @ApiProperty({
+        description: 'Information about user privileges',
+        default: false,
+    })
     @IsBoolean()
     admin: boolean;
 }
